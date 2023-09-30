eb5cbad16d54aa3d9c2585cca28612ac3c58acf9
Dimas_Wihandono <daimessdn@gmail.com>
2023-09-30 05:38:15 +0000

    created home screen page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:helloworld/screens/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,29 +15,10 @@ class MyApp extends StatelessWidget {
       // you want
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
