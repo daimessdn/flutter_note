e3ade3a5e2a0bce24b319fdbd2f56cb1354646c5
Dimas_Wihandono <daimessdn@gmail.com>
2023-09-30 05:51:30 +0000

    added physical input for create note page

--- lib/screens/create.dart
+++ lib/screens/create.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CreateNote extends StatefulWidget {
@@ -11,9 +13,27 @@ class _CreateNoteState extends State<CreateNote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Create Note"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Create Note"),
+        ),
+
+        // tambah isi add note
+        body: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  style: const TextStyle(fontSize: 28),
+                  decoration: const InputDecoration(
+                      border: InputBorder.none, hintText: "Title"),
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  style: const TextStyle(fontSize: 18),
+                  decoration: const InputDecoration(
+                      border: InputBorder.none, hintText: "Yout note here..."),
+                ),
+              ],
+            )));
   }
 }
